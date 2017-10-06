c12e6caf78bb679f6ce96586d19f11353733dc39
wbokhari <wasif.bokhari@intel.com>
2017-10-05 17:46:36 -0700

    added player view

--- src/app/poll.component.ts
+++ src/app/poll.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -10,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
 export class PollComponent implements OnInit {
 
   ngOnInit(): void {
-  }
+  };
+
+  opponent = "Avengers"; // This will come from captain
+  displayDate = Date.now();
+  displayTime = "5 PM";
 
 }
